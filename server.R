0481304d5cff8f04f027f8310fd0f88a43c3d350
Sebastian Schnell <Sebastian.Schnell@outlook.de>
2017-11-13 11:47:57 +0100

    Implement new feature of fisim package to avoid loops for simualtion

--- server.R
+++ server.R
@@ -31,61 +31,49 @@ shinyServer(function(input, output) {
 
   dt_sim_res <- reactiveValues();
   observeEvent(input$start_sim,{
-    # Collect results
-    dt_s_dstrb <- data.table(id = rep(1:input$select_sim_steps,
-                                      each = length(input$select_var)),
-                             variable = rep(input$select_var,
-                                            times = input$select_sim_steps),
-                             t_hat = 0.0,
-                             v_hat = 0.0);
 
     dens <- est_density(as.matrix(dt_pop()[, list(x_rel, y_rel)]));
     k <- ceiling(dens$max*pi*input$select_radius^2*2);
-    cnt <- 0L;
+
     withProgress(message = "Running simulation",
                  min = 0, max = 100,
                  expr = {
-                   for (m in seq.int(input$select_sim_steps)) {
-                     dt_s_loc <- xy_sample(dt_pop()[, range(x_rel)],
-                                           dt_pop()[, range(y_rel)],
-                                           input$select_sample_size);
+                   dt_s_loc <- xy_sample(dt_pop()[, range(x_rel)],
+                                         dt_pop()[, range(y_rel)],
+                                         n = input$select_sample_size,
+                                         M = input$select_sim_steps);
 
-                     s <- fixed_area(as.matrix(dt_pop()[, list(x_rel, y_rel)]),
-                                     as.matrix(dt_s_loc[, list(x_sl, y_sl)]),
-                                     r = input$select_radius,
-                                     k = k);
+                   s <- fixed_area(as.matrix(dt_pop()[, list(x_rel, y_rel)]),
+                                   as.matrix(dt_s_loc[, list(x_pt, y_pt)]),
+                                   r = input$select_radius,
+                                   k = k,
+                                   n = input$select_sample_size,
+                                   M = input$select_sim_steps);
 
-                     dt_s_tree <- extract_data(dt_pop(), s);
+                   dt_s_tree <- extract_data(dt_pop(), s);
 
-                     if (input$select_edge == "wt") {
-                       idx_wt <- edge_corr_wt(dt_s_tree, dt_s_loc, pop_border());
-                       dt_s_tree[idx_wt, f_edge := 2];
-                     }
-
-                     dt_s_plot <- sum_data(dt_s_tree, target_vars = input$select_var);
-
-                     dt_est <- est_srs(dt_s_plot);
-                     r_start <- cnt*length(input$select_var) + 1L;
-                     r_end <- r_start + length(input$select_var) - 1L;
-                     set(dt_s_dstrb,
-                         r_start:r_end,
-                         3L:4L,
-                         dt_est[, .SD, .SDcol = c("y_hat", "v_hat")]);
-                     cnt <- cnt + 1;
-                     incProgress(1/input$select_sim_steps*100);
+                   if (input$select_edge == "wt") {
+                     idx_wt <- edge_corr_wt(dt_s_tree, dt_s_loc, pop_border());
+                     dt_s_tree[idx_wt, f_edge := 2];
                    }
+
+                   dt_s_plot <- sum_data(dt_s_tree, target_vars = input$select_var);
                  });
-    dt_sim_res$a <- dt_s_dstrb;
+
+    dt_sim_res$a <- est_srs(dt_s_plot);
   });
 
   output$stand <- renderPlot({
     dt_s_loc <- xy_sample(dt_pop()[, range(x_rel)],
                           dt_pop()[, range(y_rel)],
-                          input$select_sample_size);
+                          n = input$select_sample_size,
+                          M = 1);
 
     s <- fixed_area(as.matrix(dt_pop()[, list(x_rel, y_rel)]),
-                    as.matrix(dt_s_loc[, list(x_sl, y_sl)]),
-                    r = input$select_radius);
+                    as.matrix(dt_s_loc[, list(x_pt, y_pt)]),
+                    r = input$select_radius,
+                    n = input$select_sample_size,
+                    M = 1);
 
     dt_s_tree <- extract_data(dt_pop(), s);
 
@@ -95,19 +83,19 @@ shinyServer(function(input, output) {
                   mai = c(0, 0, 0, 0))];
     dt_pop()[dt_s_tree[, stem_id],
              points(x_rel, y_rel, pch = 16)];
-    dt_s_loc[, points(x_sl, y_sl, pch = 4)];
+    dt_s_loc[, points(x_pt, y_pt, pch = 4)];
   });
 
   output$estimates <- renderTable({
     dt_sim_res$a[,
-                 list(t_hat = mean(t_hat),
-                      v_emp = var(t_hat),
+                 list(y_hat = mean(y_hat),
+                      v_emp = var(y_hat),
                       v_hat = mean(v_hat)),
                  by = variable];
   });
 
   output$distribution <- renderPlot({
-    ggplot(dt_sim_res$a, aes(t_hat)) +
+    ggplot(dt_sim_res$a, aes(y_hat)) +
       geom_line(stat = 'density') +
       facet_wrap( ~ variable, scales = "free", ncol = 2) +
       theme_light()
